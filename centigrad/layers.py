038b9d06a13a05446861533be7ee93d5f725025c
pierresquarra <pierre@squarra.de>
2023-09-21 00:14:10 +0200

    cleaned layers and added tests

---
+++ centigrad/layers.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+from centigrad.engine import Value
+from centigrad.nn import Module
+
+
+class _Layer(Module):
+    """Private base class for neural network layers"""
+
+    def __call__(self, inputs: np.ndarray):
+        """Applies the layer transformation to the input."""
+        raise NotImplementedError
+
+    def forward(self, inputs: np.ndarray):
+        """Alias for the __call__ method, applying the layer transformation to the input."""
+        return self(inputs)
+
+    def parameters(self):
+        """Gets the parameters of the layer."""
+        return super().parameters()
+
+
+class Linear(_Layer):
+    """Simple linear (dense) layer which applies a linear transformation to the input data."""
+
+    def __init__(self, in_features, out_features, bias=True):
+        k = np.sqrt(1 / in_features)
+        self.weight = np.vectorize(Value)(np.random.uniform(low=-k, high=k, size=(out_features, in_features)))
+        self.bias = np.vectorize(Value)(np.random.uniform(low=-k, high=k, size=(out_features,))) if bias else None
+
+    def __call__(self, inputs: np.ndarray):
+        return np.dot(inputs, self.weight.T) + (self.bias if self.bias is not None else 0)
+
+    def parameters(self):
+        return self.weight.flatten().tolist() + (self.bias.tolist() if self.bias is not None else [])
+
+
+class Tanh(_Layer):
+    """Layer that applies the hyperbolic tangent activation function element-wise to the input data."""
+
+    def __call__(self, inputs: np.ndarray):
+        return np.tanh(inputs)
+
+
+class ReLU(_Layer):
+    """Layer that applies the Rectified Linear Unit (ReLU) activation function element-wise to the input data."""
+
+    def __call__(self, inputs: np.ndarray):
+        return np.vectorize(Value.relu)(inputs)
+
+
+class Sigmoid(_Layer):
+    """Layer that applies the sigmoid activation function element-wise to the input data."""
+
+    def __call__(self, inputs: np.ndarray):
+        return np.vectorize(Value.sigmoid)(inputs)
+
+
+class Softmax(_Layer):
+    """Layer that applies the softmax activation function to the input data"""
+
+    def __call__(self, inputs: np.ndarray):
+        return np.exp(inputs) / np.sum(np.exp(inputs), axis=1, keepdims=True)
+
+
+class LogSoftmax(_Layer):
+    """A layer that applies the log-softmax activation function to the input data"""
+
+    def __call__(self, inputs: np.ndarray):
+        return np.log(np.exp(inputs) / np.sum(np.exp(inputs), axis=1, keepdims=True))
---
+++ tests/test_layers.py
@@ -0,0 +1,87 @@
+from unittest import TestCase
+
+import numpy as np
+import torch
+from torch import nn
+
+from centigrad.engine import Value
+from centigrad.layers import Linear, Tanh, ReLU, Softmax, Sigmoid, LogSoftmax
+
+
+class TestLinear(TestCase):
+    def test_with_bias(self):
+        pt = nn.Linear(20, 30)
+
+        pt_input = torch.randn(128, 20)
+        pt_output = pt(pt_input)
+
+        cg = Linear(20, 30)
+        cg.weight = pt.weight.detach().numpy()
+        cg.bias = pt.bias.detach().numpy()
+
+        cg_input = pt_input.detach().numpy()
+        cg_output = torch.tensor(cg(cg_input))
+
+        self.assertEqual(pt_output.shape, cg_output.shape)
+        self.assertTrue(torch.allclose(pt_output, cg_output, rtol=1e-3))
+
+    def test_without_bias(self):
+        pt = nn.Linear(20, 30, bias=False)
+
+        pt_input = torch.randn(128, 20)
+        pt_output = pt(pt_input)
+
+        cg = Linear(20, 30, bias=False)
+        cg.weight = pt.weight.detach().numpy()
+
+        cg_input = pt_input.detach().numpy()
+        cg_output = torch.tensor(cg(cg_input))
+
+        self.assertEqual(pt_output.shape, cg_output.shape)
+        self.assertTrue(torch.allclose(pt_output, cg_output, rtol=1e-3))
+
+
+class TestActivations(TestCase):
+    def setUp(self) -> None:
+        self.pt_in = torch.randn(5, 12)
+        self.cg_in = np.vectorize(Value)(self.pt_in.detach().numpy())
+
+    def test_tanh(self):
+        pt = nn.Tanh()
+        pt_out = pt(self.pt_in)
+        cg = Tanh()
+        cg_out = cg(self.cg_in)
+        cg_ten = torch.tensor(np.vectorize(lambda x: x.data)(cg_out)).float()
+        self.assertTrue(torch.allclose(pt_out, cg_ten, rtol=1e-3))
+
+    def test_relu(self):
+        pt = nn.ReLU()
+        pt_out = pt(self.pt_in)
+        cg = ReLU()
+        cg_out = cg(self.cg_in)
+        cg_ten = torch.tensor(np.vectorize(lambda x: x.data)(cg_out)).float()
+        self.assertTrue(torch.allclose(pt_out, cg_ten, rtol=1e-3))
+
+    def test_sigmoid(self):
+        pt = nn.Sigmoid()
+        pt_out = pt(self.pt_in)
+        cg = Sigmoid()
+        cg_out = cg(self.cg_in)
+        cg_ten = torch.tensor(np.vectorize(lambda x: x.data)(cg_out)).float()
+        self.assertTrue(torch.allclose(pt_out, cg_ten, rtol=1e-3))
+
+    def test_softmax(self):
+        pt = nn.Softmax(dim=1)
+        pt_out = pt(self.pt_in)
+        cg = Softmax()
+        cg_out = cg(self.cg_in)
+        cg_ten = torch.tensor(np.vectorize(lambda x: x.data)(cg_out)).float()
+        self.assertTrue(torch.allclose(pt_out, cg_ten, rtol=1e-3))
+
+    def test_log_softmax(self):
+        pt = nn.LogSoftmax(dim=1)
+        pt_out = pt(self.pt_in)
+        cg = LogSoftmax()
+        cg_out = cg(self.cg_in)
+        cg_ten = torch.tensor(np.vectorize(lambda x: x.data)(cg_out)).float()
+        self.assertTrue(torch.allclose(pt_out, cg_ten, rtol=1e-3))
