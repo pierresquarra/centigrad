fd43b5b356052ee532d5a6669765de415676a005
pierresquarra <pierre@squarra.de>
2023-09-25 14:46:04 +0200

    added NLLLoss

--- centigrad/losses.py
+++ centigrad/losses.py
@@ -36,3 +36,13 @@ class HingeLoss(_Loss):
         """Computes the hinge loss between predictions and targets."""
         assert predictions.shape == targets.shape, f"shapes {predictions.shape} and {targets.shape} don't align"
         return np.mean(np.maximum(0, (1 - targets * predictions)))
+
+
+class NLLLoss(_Loss):
+    """Negative Log Likelihood (NLL) loss function."""
+
+    def __call__(self, predictions: np.ndarray, targets: np.ndarray):
+        """Computes the NLL loss between log predictions and targets."""
+        assert predictions.ndim == 2, f"Expected 2D array, got shape {predictions.shape}"
+        assert predictions.shape[0] == targets.shape[0], f"shapes {predictions.shape[0]} and {targets.shape[0]} don't align"
+        return -np.mean(predictions[np.arange(predictions.shape[0]), targets])
